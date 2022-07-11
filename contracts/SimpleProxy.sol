53ab3bfc5a8755dcccdb3a015946042f4f484953
Melvillian <melvillevt@gmail.com>
2022-07-11 16:43:49 -0400

    initial impls of simple proxy and logic

---
+++ contracts/SimpleProxy.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract SimpleProxy {
+
+    /// @dev The contract address that the SimpleProxy will delegate all functions calls to
+    address public logicContract;
+
+    constructor(address _logicContract) {
+        logicContract = _logicContract;
+    }
+
+    /// @notice Points this Proxy's logic contract address to a new contract,
+    /// allowing the Proxy to upgrade itself
+    function upgrade(address _newLogic) external {
+        logicContract = _newLogic;
+    }
+
+    fallback() external payable {
+        delegateTheCall();
+    }
+
+    receive() external payable {
+        delegateTheCall();
+    }
+
+    /// @dev This is the most important Proxy logic. In it, we forward the function call
+    /// (msg.data, which contains the 4 byte function selector as well as the function arguments)
+    /// to the logic contract using `delegatecall`. `delegatecall` will cause whatever function
+    /// gets called to use the msg.sender, msg.value, and msg.data from the Proxy's context but
+    /// use the code from the Logic contract
+    function delegateTheCall() internal {
+        (bool success, bytes memory data) = address(logicContract).delegatecall(msg.data);
+
+        if (!success) {
+            revert(string(data));
+        }
+
+        // in this simple example, we don't handle the return data; it is lost :(
+    }
+}
